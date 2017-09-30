d3734d6308c87bc370a6e1000602afa7cae3396b
Andre Godinez <a.godinez1@nuigalway.ie>
2017-09-30 15:36:31 +0100

    added q3

--- Assignment 3/assignment3_Andre_Godinez/Test.java
+++ Assignment 3/assignment3_Andre_Godinez/Test.java
@@ -10,29 +10,30 @@ public class Test {
 	public static void main(String args[]) throws IOException, ClassNotFoundException {
 		String directory = System.getProperty("user.dir"); 
 		
-//		Transaction[] transactions = new Transaction[3];
-//		
-//		transactions[0] = new Transaction("16/08/2017","Open Account",100);
-//		transactions[1] = new Transaction("22/08/2017","Withdraw",100);
-//		transactions[2] = new Transaction("23/09/2017","Deposit",100);
-//		
-//		FileOutputStream fos = new FileOutputStream("transactions.txt");
-//		ObjectOutputStream oos = new ObjectOutputStream(fos);
-//		
-//			System.out.println("Serializing array of objects to " +directory);
-//			oos.writeObject(transactions);
-//		
-//		
-//		oos.close();
-//		
-//		FileInputStream fis = new FileInputStream("transactions.txt");
-//		ObjectInputStream ois = new ObjectInputStream(fis);
-//		
-//		Transaction[] transactionsCopy = (Transaction[]) ois.readObject();
-//		
-//		for(int i=0;i<transactionsCopy.length;i++) {
-//			System.out.println(transactionsCopy[i].toString());
-//		}
+		System.out.println("Question 1 :\n\n");
+		Transaction[] transactions = new Transaction[3];
+		
+		transactions[0] = new Transaction("16/08/2017","Open Account",100);
+		transactions[1] = new Transaction("22/08/2017","Withdraw",100);
+		transactions[2] = new Transaction("23/09/2017","Deposit",100);
+		
+		
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("transactions.txt"));
+		
+		System.out.println("Serializing array of objects to " +directory);
+		oos.writeObject(transactions);
+		
+	
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("transactions.txt"));
+		System.out.println("Reading array of objects from " + directory);
+		Transaction[] transactionsCopy = (Transaction[]) ois.readObject();
+		
+		for(int i=0;i<transactionsCopy.length;i++) {
+			System.out.println(transactionsCopy[i].toString());
+		}
+		
+		System.out.println("\n\nQuestion 2 :\n\n ");
+		
 		System.out.println("Opening Bank account...\n");
 		BankAccount ba = new BankAccount("Jenny Lee","16/08/2017",100);
 		System.out.println(ba.toString()+"\n");
@@ -47,22 +48,44 @@ public class Test {
 		
 		System.out.println(ba.getTransactionDetail());
 		
-		FileOutputStream fos = new FileOutputStream("accountDetails.txt");
-		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		
+		ObjectOutputStream oos1 = new ObjectOutputStream(new FileOutputStream("accountDetails.txt"));
 		
 		System.out.println("Serializing Bank Account object to " + directory + "\n");
 		oos.writeObject(ba);
 		
-		oos.close();
+		
+		
 		
 		System.out.println("Retrieving serialized Bank Account object from " + directory + "\n");
-		FileInputStream fis = new FileInputStream("accountDetails.txt");
-		ObjectInputStream ois = new ObjectInputStream(fis);
+		
+		ObjectInputStream ois1 = new ObjectInputStream(new FileInputStream("accountDetails.txt"));
 		
 		BankAccount baCopy = (BankAccount) ois.readObject();
 		
 		System.out.println(baCopy.toString()+"\n");
 		System.out.println(baCopy.getTransactionDetail());
+		
+		System.out.println("\n\nQuestion 3 :\n\n");
+		 // create a new RandomAccessFile with filename test
+        RandomAccessFile file = new RandomAccessFile(directory +"/question3.txt", "rw");
+       
+        file.seek(file.length());
+        file.writeChars("Yes");
+        
+        
+        file.seek(0);
+        String line;
+        if((line = file.readLine()) != null) {
+          System.out.println(line);
+        }
+        
+        oos.close();
+		oos1.close();
+		ois.close();
+		ois1.close();
+        file.close();
+       
 	}
 	
 }
